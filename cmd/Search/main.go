c35276cffcbae44198a84e883dbec6bea823adda
avimitin <avimitin@gmail.com>
2020-10-18 13:14:42 +0800

    add args support

--- cmd/Search/main.go
+++ cmd/Search/main.go
@@ -3,13 +3,24 @@ package main
 import (
 	"fmt"
 	"time"
-
+	"os"
 	"github.com/Avimitin/SearchFile/internal/search"
 )
 
 func main() {
-	target := "test"
-	orderPath := "/home/avimitin"
+	if length := len(os.Args); length != 3 {
+		fmt.Printf("search need 2 args but got only %d\n", length-1)
+		os.Exit(-1)
+	}
+
+	target := os.Args[1]
+	orderPath := os.Args[2]
+	fmt.Printf("target: %s\norderpath:%s\nDo you want to continue? [y/N] ", target, orderPath)
+	var confirm string
+	fmt.Scanf("%s", confirm)
+	if confirm == "n" || confirm == "N" {
+		os.Exit(0)
+	}
 
 	fmt.Println("===Using one thread===")
 	start := time.Now()
